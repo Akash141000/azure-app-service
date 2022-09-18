8cac4a270cf4e813500499fe8213247e22c735c4
Akash Sanjay Padwal <akashsanjaypadwal@Akashs-MacBook-Pro.local>
2022-09-18 17:46:00 +0530

    testing action again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! ${process.env.PORT || 'No port'}`;
+    return `Hello World! 1 ${process.env.PORT || 'No port'}`;
   }
 }
